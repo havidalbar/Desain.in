1e6cb56ec63117810bf7d5fd29351093872310e3
havidalbar@gmail.com <havidalbar@gmail.com>
2019-08-24 22:15:28 +0700

    add components footer and navbar

---
+++ frontend/src/components/LeftMenu.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import { Menu,Anchor } from 'antd';
+const SubMenu = Menu.SubMenu;
+const MenuItemGroup = Menu.ItemGroup;
+
+class LeftMenu extends Component {
+    render() {
+        return (
+            <Menu mode={this.props.mode}>
+                <Menu.Item key="mail">
+                    <Anchor href="">Home</Anchor>
+                </Menu.Item>
+                <Menu.Item key="alipay">
+                    <Anchor href="">Contact Us</Anchor>
+                </Menu.Item>
+                <SubMenu title={<span>Blogs</span>}>
+                    <MenuItemGroup title="Item 1">
+                        <Menu.Item key="setting:1">Option 1</Menu.Item>
+                        <Menu.Item key="setting:2">Option 2</Menu.Item>
+                    </MenuItemGroup>
+                    <MenuItemGroup title="Item 2">
+                        <Menu.Item key="setting:3">Option 3</Menu.Item>
+                        <Menu.Item key="setting:4">Option 4</Menu.Item>
+                    </MenuItemGroup>
+                </SubMenu>
+            </Menu>
+        );
+    }
+}
+
+export default LeftMenu;
