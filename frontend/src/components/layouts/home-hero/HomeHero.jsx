12d360578a441ff1b570e98b61ee00dbaf787712
Gilang Nur A'idi <nuraidigilang@gmail.com>
2019-09-04 13:16:45 +0700

    Adding home hero
    HomeHero.jsx homeHero.css

--- frontend/src/components/layouts/home-hero/HomeHero.jsx
+++ frontend/src/components/layouts/home-hero/HomeHero.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Carousel } from 'antd';
+import { Row, Col, Carousel } from 'antd';
 import PrimaryButton from '../../button/Primary';
 import './homeHero.scss';
 
@@ -7,7 +7,45 @@ class HomeHero extends Component {
     render() {
         return(
             <div className="hero">
-                
+                <Row gutter={40}>
+                    <Col span={18}>
+                        <Carousel autoplay  effect="fade">
+                            <div>
+                                <h3>1</h3>
+                            </div>
+                            <div>
+                                <h3>2</h3>
+                            </div>
+                            <div>
+                                <h3>3</h3>
+                            </div>
+                            <div>
+                                <h3>4</h3>
+                            </div>
+                        </Carousel>
+                    </Col>
+                    <Col className="side-hero" span={6} gutter={8}>
+                        <Row type="flex" justify="space-between">
+                            <div className="side-menu-item">
+                                <span className="side-menu-title">
+                                    PORTFOLIO
+                                </span>
+                            </div>    
+                       
+                            <div className="side-menu-item">
+                                <span className="side-menu-title">
+                                    PORTFOLIO
+                                </span>
+                            </div>    
+                       
+                            <div className="side-menu-item">
+                                <span className="side-menu-title">
+                                    PORTFOLIO
+                                </span>
+                            </div>    
+                        </Row>
+                    </Col>
+                </Row>
             </div>
         );
     };
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import Footer from '../components/layouts/footer/Footer';
 import NavBar from '../components/layouts/navbar/NavBar';
+import HomeHero from '../components/layouts/home-hero/HomeHero';
 
 class App extends Component {
   render() {
     return (
         <div>
             <NavBar/>
+            <HomeHero/>
             <Footer/>
         </div>
     );
