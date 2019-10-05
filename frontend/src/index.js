1f406d66cca93e02546b76fd76f0e14e5c328691
havidalbar@gmail.com <havidalbar@gmail.com>
2019-10-05 17:02:27 +0700

    fixed conflict

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -6,11 +6,7 @@ import Kontes from './pages/contest/KontesPenyelenggara';
 import 'antd/dist/antd.css';
 import * as serviceWorker from './serviceWorker';
 
-<<<<<<< HEAD
 ReactDOM.render(<Profile />, document.getElementById('root'));
-=======
-ReactDOM.render(<Kontes />, document.getElementById('root'));
->>>>>>> 236539fda88471b46fc6953d9cd6c03c20cebe9d
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() belo1w. Note this comes with some pitfalls.
