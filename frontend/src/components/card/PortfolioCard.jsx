afec3dd06a02ddf8299a04d3ae8a697f36e36e5e
unknown <graha.asa@gmail.com>
2019-10-05 23:00:28 +0700

    Close modal

--- frontend/src/components/card/PortfolioCard.jsx
+++ frontend/src/components/card/PortfolioCard.jsx
@@ -1,10 +1,16 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
-import Modal from '../../pages/Modal';
-import { Icon } from 'antd';
 import './cstmCard.scss';
 import '../layouts/typography.scss';
+import { Modal, Icon } from 'antd';
+import '../../components/layouts/home/modal.scss';
+
+
+  
+  
+  
+  
 
 class CardPortfolio extends Component {
     state = { visible: false };
@@ -17,8 +23,46 @@ class CardPortfolio extends Component {
 
     render() {
         return (
-            <div className="card-portfolio">
-                <Modal visible={this.state.visible} title={this.props.title} image={this.props.imageSrc} likeCount={this.props.likeCount} viewCount={this.props.viewCount} />
+            <div className="card-portfolio" >
+                <Modal 
+                visible={this.state.visible} 
+                destroyOnClose={true}
+                title={this.props.title} 
+                onCancel={()=>{
+            this.setState({visible:false}) 
+          }}
+          onOk={()=>{
+            this.setState({visible:false}) 
+          }}
+          onCancel={()=>{
+            this.setState({visible:false}) 
+          }}
+          >
+                <p className="title-3">
+              {this.props.title}
+            </p>
+            <div className="image-cover">
+              <img className="modal-image" src={this.props.imageSrc} />
+            </div>
+            <div className="modal-content">
+              <p className="regular-body-text">
+                I’ve got what they call an “active imagination.” Sense forever I’ve been creeped out in the dark.
+                It’s like a constant feeling of being chased. Turning stomach, sweaty hands and panicky thoughts.
+                Growing up in a Christian home I’d always go to Jesus when I got scared and he’d always help. Even when our relationship was not in a good spot.
+              </p>
+              <div className="card-action">
+                <div className="indicator view">
+                  <Icon type="eye" className="icn-wrap" />
+                  <p className="ind-content">{this.props.viewCount} views</p>
+                </div>
+                <div className="indicator like">
+                  <Icon type="heart" className="icn-wrap" />
+                  <p className="ind-content">{this.props.likeCount} likes</p>
+                </div>
+              </div>
+            </div>
+                
+                </Modal>
                 <div className="image-cover">
                     <img className="img" src={this.props.imageSrc} onClick={this.showModal} />
                 </div>
