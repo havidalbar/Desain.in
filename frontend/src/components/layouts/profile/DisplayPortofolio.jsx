ee5817c354e1d3e6a5c2686a8f0847e1f09616d9
havidalbar@gmail.com <havidalbar@gmail.com>
2019-10-12 02:40:57 +0700

    fixed

--- frontend/src/components/layouts/profile/DisplayPortofolio.jsx
+++ frontend/src/components/layouts/profile/DisplayPortofolio.jsx
@@ -17,7 +17,7 @@ class DisplayPortofolio extends Component {
                     <PortofolioCard judul="Fishception - Warmup" userName="Muhamad Wildan Aldiansyah" likeCount="221" viewCount="432" imageSrc="https://storage.cloud.google.com/aldiwildan_bucket/foto-026.jpg"/>
                     <PortofolioCard judul="Vectober 04 - Freeze" userName="M Havid Albar P" likeCount="190" viewCount="320" imageSrc="https://storage.cloud.google.com/aldiwildan_bucket/foto-002.jpg"/>
                     <PortofolioCard judul="Mr Florist" userName="Gilang Nur A'idi" likeCount="175" viewCount="234" imageSrc="https://storage.cloud.google.com/aldiwildan_bucket/foto-003.jpg"/>
-                    <PortofolioCard judul="GridFriday Fork" userName="Adam Sulthoni" likeCount="199" viewCount="297" imageSrc="https://storage.cloud.google.com/aldiwildan_bucket/foto-004.jpg"/>
+                    <PortofolioCard judul="GridFriday Fork" userName="Adam Sulthoni" likeCount="199" viewCount="297" imageSrc="https://storage.cloud.google.com/aldiwildan_bucket/foto-025.png"/>
                 </div>
                 <div className="btn-wrap">
                     <Button style="button primary" text="LIHAT LEBIH BANYAK" />
